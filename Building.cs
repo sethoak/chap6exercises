3a9e17a768216831ba19d14a01a9cabe99d46d03
sethoak <sethoak@gmail.com>
2019-11-18 15:09:56 -0600

    saving code

--- Building.cs
+++ Building.cs
@@ -1,16 +1,19 @@
+using System;
+
 namespace Planner
 {
     public class Building
     {
-        private string _designer
-        private DateTime _dateConstructed
-        private string _address
-        private string _owner
+        // private properties
+        private string _designer;
+        private DateTime _dateConstructed;
+        private string _address;
+        private string _owner;
 
-        public int Stories { get; set; };
-
-        public double Width { get; set; };
-        public double Depth { get; set; };
+        // public properties
+        public int Stories { get; set; }
+        public double Width { get; set; }
+        public double Depth { get; set; }
         public double Volume
         {
             get
@@ -18,29 +21,36 @@ namespace Planner
                 return Width * Depth * (3 * Stories);
             }
         }
-        //constructor
+
+        // constructor 
         public Building(string address)
         {
             _address = address;
         }
 
-        //Define a Construct() method.
-        //The method's logic should set the _dateConstructed field's value to DateTime.Now.
-
+        // Define a Construct() method. The method's logic should set the _dateConstructed field's value to DateTime.Now.
         public void Construct(string designer)
         {
             _designer = designer;
-            _dateConstructed = dateConstructed;
+            _dateConstructed = DateTime.Now;
         }
-
-        //Define a Purchase() method.
-        //The method should accept a single string argument of the name of the person purchasing a building. The method should take that string and assign it to the private _owner field.
-
+        // Define a Purchase() method. The method should accept a single string argument of the name of the person purchasing a building. The method should take that string and assign it to the private _owner field
         public void Purchase(string owner)
         {
             _owner = owner;
         }
 
+        public void PrintDetails()
+        {
+            Console.WriteLine("==============================");
+            Console.WriteLine(_address);
+            Console.WriteLine("------------------------------");
+            Console.WriteLine($"Designed by {_designer}");
+            Console.WriteLine($"Constructed on {_dateConstructed}");
+            Console.WriteLine($"Owned by {_owner}");
+            Console.WriteLine($"{Volume} cubic meters of space");
+            Console.WriteLine();
+        }
     }
 
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Planner
 {
@@ -6,7 +7,78 @@ namespace Planner
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            City Gotham = new City();
+            // Create several buildings in the Main() method of Program.cs.
+            Building FiveOneTwoEigth = new Building("512 8th Avenue");
+            Building BatmanBuilding = new Building("123 Downtown Avenue");
+            Building CatwomanBuilding = new Building("999 Matriarchy Drive");
+            Building CarnegieHall = new Building("Practice Practice Practice");
+            Building GooGooHaim = new Building("1000 Big Museum Blvd");
+            Building NashvilleSoftWareSchool = new Building("??? Good HVAC Street");
+            // Give each building a width, height, and number of stories.
+            FiveOneTwoEigth.Width = 100;
+            FiveOneTwoEigth.Stories = 10;
+            FiveOneTwoEigth.Depth = 75;
+
+            BatmanBuilding.Width = 345;
+            BatmanBuilding.Stories = 94;
+            BatmanBuilding.Depth = 234;
+
+            CatwomanBuilding.Width = 123;
+            CatwomanBuilding.Stories = 87;
+            CatwomanBuilding.Depth = 400;
+
+            CarnegieHall.Width = 6329;
+            CarnegieHall.Stories = 40;
+            CarnegieHall.Depth = 25;
+
+            GooGooHaim.Width = 126;
+            GooGooHaim.Stories = 60;
+            GooGooHaim.Depth = 12;
+
+            NashvilleSoftWareSchool.Width = 30;
+            NashvilleSoftWareSchool.Stories = 23;
+            NashvilleSoftWareSchool.Depth = 100;
+            // Construct each building.
+            FiveOneTwoEigth.Construct("Frank LLoyd Wright");
+            BatmanBuilding.Construct("Alfred");
+            CatwomanBuilding.Construct("Michele Pfeifer");
+            NashvilleSoftWareSchool.Construct("John Wark");
+            CarnegieHall.Construct("Some Nerd");
+            GooGooHaim.Construct("Frank Garhehehe");
+            // Have business people in your city purchase each of your buildings.
+            FiveOneTwoEigth.Purchase("Blimp");
+            BatmanBuilding.Purchase("Brample");
+            CatwomanBuilding.Purchase("Repples");
+            NashvilleSoftWareSchool.Purchase("Chip");
+            CarnegieHall.Purchase("Flap");
+            GooGooHaim.Purchase("John");
+            // After all of the buildings have been purchased, display the following information to the console for each building.
+            List<Building> buildings = new List<Building>()
+            {
+                FiveOneTwoEigth,
+                BatmanBuilding,
+                CatwomanBuilding,
+                NashvilleSoftWareSchool,
+                CarnegieHall,
+                GooGooHaim
+            };
+
+            Gotham.Buildings = buildings;
+
+            foreach (Building building in buildings)
+            {
+                building.PrintDetails();
+            }
+
+            Building HouseHome = new Building("1234 Where the Heart Is Rd");
+            Gotham.AddBuilding(HouseHome);
+
+            foreach (Building building in buildings)
+            {
+                building.PrintDetails();
+            }
+
         }
     }
 }
